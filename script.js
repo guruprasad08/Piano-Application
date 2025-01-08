a0a558befac92e63fb3cddcc8bee2ebefff23d08
gp <guruprasad26gp@gmail.com>
2025-01-08 09:44:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const keys =document.querySelectorAll(".key");
+
+const whitekey = document.querySelectorAll(".key.white");
+const blackkey = document.querySelectorAll(".key.black");
+
+keys.forEach(key => key.addEventListener("click", handleKeyClick));
+
+function handleKeyClick() {
+    playKey(this);
+}
+
+function playKey(key) {
+    const keyAudio = document.getElementById(key.dataset.note);
+    keyAudio.currentTime = 0;
+    keyAudio.play();
+    key.classList.add("active");
+    keyAudio.addEventListener("ended",()=>{
+        key.classList.remove("active");
+    });
+}
